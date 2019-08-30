bcfa174526a91f34488edeffa025ddd0ee48a030
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-08-30 14:32:31 +0100

    Gracefully handling process exit in server

--- service/src/server.ts
+++ service/src/server.ts
@@ -1,9 +1,13 @@
 import app from './app';
 import * as db from './db';
 
-db.init().then(() => {
-    app.listen(app.get('port'), () => {
-      console.log('Running at http://127.0.0.1:%d', app.get('port'));
-    });
-  }
-);
+db.init().then(() => console.log('Database initialised'));
+
+const server = app.listen(app.get('port'), () => {
+  console.log('Running at http://127.0.0.1:%d', app.get('port'));
+});
+
+process.on('exit', () => {
+  db.close();
+  server.close();
+});
