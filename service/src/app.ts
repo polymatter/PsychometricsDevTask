65c2226620a0027ccdf4b2e35fa901486784c021
Bartosz Kielczewski <b.kielczewski@jbs.cam.ac.uk>
2019-09-02 15:49:17 +0100

    Added cors

--- service/src/app.ts
+++ service/src/app.ts
@@ -1,5 +1,6 @@
 import * as bodyParser from 'body-parser';
 import compression from 'compression';
+import cors from 'cors';
 import express from 'express';
 import * as userController from './user/user.controller';
 
@@ -8,14 +9,15 @@ const app = express();
 app.set('port', process.env.PORT || 8080);
 
 app.use(compression());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.set('json spaces', 2);
 
 app.get('/users', userController.getPage);
 app.get('/users/:userId', userController.get);
-app.post('/users', userController.save);
-app.put('/users/:userId', userController.save);
+app.post('/users', userController.create);
+app.put('/users/:userId', userController.update);
 app.delete('/users/:userId', userController.deleteOne);
 
 export default app;
